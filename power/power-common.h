560bf5b6b744b374e02df6df1738393c6c7e930a
tomascus <arbiter1000@gmail.com>
2019-02-19 17:15:58 +1100

    msm8937-common: power: Account for activity launch end hint
    Currently we unconditionally boost as long as POWER_HINT_LAUNCH is
    received. However, the framework sends the powerhint for when launch
    starts and when launch finishes [1], and thereby causing the boosting
    to last twice as long as we wanted.
    
    1) Check whether the hint is a start or finish hint
    2) Make use of launch finish hint to signal when to release the perflock
    and don't rely on fixed duration boosting which has two shortcomings:
     * perflock can be held for too long, causing the CPU freq and other resources
       to be boosted for too long even if the application has finished launching,
       resulting in excessive battery drainage
     * perflock can be held for not long enough if it takes more than the timeout
       time to finish launch
    
    [1]: https://github.com/LineageOS/android_frameworks_base/commit/65c7a15833b61bf2a3d193992326a68716f0aca4
    
    Reference: Wahoo power-libperfmgr
    
    Change-Id: I405cc453c5f58d9fb2583b9c6017f3964a0ce024

--- power/power-common.h
+++ power/power-common.h
@@ -54,6 +54,16 @@ enum CPU_GOV_CHECK {
     CPU3 = 3
 };
 
+enum {
+    PROFILE_POWER_SAVE = 0,
+    PROFILE_BALANCED,
+    PROFILE_HIGH_PERFORMANCE,
+    PROFILE_BIAS_POWER,
+    PROFILE_BIAS_PERFORMANCE
+};
+
+#define CHECK_HANDLE(x) ((x)>0)
+
 #define UNUSED(x) UNUSED_ ## x __attribute__((__unused__))
 
 // Custom Lineage hints
